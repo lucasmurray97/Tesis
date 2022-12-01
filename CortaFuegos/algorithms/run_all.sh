119d1a48381775e9cd0152183eb7a1e7729e609c
Lucas Murray <lucas@PC-Lu.localdomain>
2022-12-01 15:39:47 -0300

    improved code performance

--- CortaFuegos/algorithms/run_all.sh
+++ CortaFuegos/algorithms/run_all.sh
@@ -2,8 +2,21 @@
 # Full Grid
 # Sub 6x6
 # MAB GREEDY
-#python3 algorithm_analysis.py --algorithm mab_greedy --size 6 --env full_grid --env_version v1 --episodes 100 --window 10 --instance sub6x6
+python3 algorithm_analysis.py --algorithm mab_greedy --size 6 --env full_grid --env_version v1 --episodes 500 --window 100 --instance sub6x6 --epsilon 0.1
 # MAB UCB
 #python3 algorithm_analysis.py --algorithm mab_ucb --size 6 --env full_grid --env_version v1 --episodes 100 --window 100 --epsilon 0.1 --instance sub6x6
 #Q_Learning
-python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 50 --alpha 2e-4 --epsilon 0.1 --instance sub6x6
+python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 100  --alpha 2e-2 --epsilon 0.1 --instance sub6x6
+python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 100  --alpha 2e-3 --epsilon 0.1 --instance sub6x6
+python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 100  --alpha 2e-4 --epsilon 0.1 --instance sub6x6
+python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 100  --alpha 2e-5 --epsilon 0.1 --instance sub6x6
+
+python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 100  --alpha 2e-2 --epsilon 0.15 --instance sub6x6
+python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 100  --alpha 2e-3 --epsilon 0.15 --instance sub6x6
+python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 100  --alpha 2e-4 --epsilon 0.15 --instance sub6x6
+python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 100  --alpha 2e-5 --epsilon 0.15 --instance sub6x6
+
+python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 100  --alpha 2e-2 --epsilon 0.2 --instance sub6x6
+python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 100  --alpha 2e-3 --epsilon 0.2 --instance sub6x6
+python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 100  --alpha 2e-4 --epsilon 0.2 --instance sub6x6
+python3 algorithm_analysis.py --algorithm q_learning --size 6 --env full_grid --env_version v1 --episodes 1000 --window 100  --alpha 2e-5 --epsilon 0.2 --instance sub6x6
