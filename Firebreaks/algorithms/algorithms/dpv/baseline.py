63c7791449f739f5398dde073354dc0bc092ffab
lucas <lucasmurrayh@gmail.com>
2023-02-03 10:33:54 -0300

    fixed create_demon

--- Firebreaks/algorithms/algorithms/dpv/baseline.py
+++ Firebreaks/algorithms/algorithms/dpv/baseline.py
@@ -144,9 +144,10 @@ def generate_demonstrations(episodes, size, n_sims, n_sims_eval, env, version):
         while not done:
             step_data = [state.clone().tolist().copy(), episode[j][0], episode[j][1], episode[j][2]]
             next_state, _, done = env.step(torch.Tensor([episode[j][0]]))
+            step_data.append(next_state.clone().tolist().copy())
+            print(step_data[0], step_data[4])
             if done:
                 next_state = env.reset()
-            step_data.append(next_state.clone().tolist().copy())
             data[i][step] = step_data
             state = next_state
             step += 1
