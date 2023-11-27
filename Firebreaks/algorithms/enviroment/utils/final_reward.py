318d9f77d5c777ba8aeb02e935061bd9732b85e0
Lucas Murray <lucasmurrayh@gmail.com>
2023-11-27 11:25:06 -0300

    (fix): updated import path

--- Firebreaks/algorithms/enviroment/utils/final_reward.py
+++ Firebreaks/algorithms/enviroment/utils/final_reward.py
@@ -5,7 +5,8 @@ import csv
 import pprint
 import os
 # We add the path to the simulator in order to execute it
-sys.path.append("../../../../Simulador/Cell2Fire/Cell2Fire/")
+sys.path.append("../../../../Simulador/Cell2Fire/")
+sys.path.append("../../Simulador/Cell2Fire/")
 from cell2fire.Cell2FireC_class import Cell2FireC
 from cell2fire.main import main
 import os
